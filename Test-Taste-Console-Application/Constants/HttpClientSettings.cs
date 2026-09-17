66159658cd913518ef8e18086fe7b3c02272aa0d
Priyal Dalal <84765716+priyaldalal@users.noreply.github.com>
2026-09-17 15:40:46 +0530

    Add API key generate from website

--- Test-Taste-Console-Application/Constants/HttpClientSettings.cs
+++ Test-Taste-Console-Application/Constants/HttpClientSettings.cs
@@ -1,7 +1,8 @@
-﻿namespace Test_Taste_Console_Application.Constants
+namespace Test_Taste_Console_Application.Constants
 {
     public static class HttpClientSettings
     {
         public const string JsonType = "application/json";
+        public const string ApiKey = "4b16e39f-70f1-49ed-8cb5-4725580be417";
     }
 }
